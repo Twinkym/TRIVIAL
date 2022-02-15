19b611f150e59546964f4aa34d5e6b4b6ec91417
david de la puente enriquez <Twinkymen@hotmail.com>
2022-02-15 11:35:15 +0100

    Hola Preguntas en el Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola amigos :) esto se convertirá " +
                 "en un Trivial Game en un Juego del Trivial");
-
+        Scanner sc = new Scanner(System.in);
 
         List<Preguntas> preguntas = buildPreguntasList();
 
@@ -14,5 +15,11 @@ public class Main {
 
     private static List<Preguntas> buildPreguntasList() {
         List<Preguntas> preguntas = new ArrayList<>();
+
+        preguntas.add(new Preguntas("¿? S/N "));
+        preguntas.add(new Preguntas("¿Java es difícil? S/N "));
+        preguntas.add(new Preguntas(" ¿Java es un lenguaje de programación? S/N "));
+        preguntas.add(new Preguntas(" ¿Programar es dificil? S/N "));
+        preguntas.add(new Preguntas(" ¿Java es un lenguaje POO? S/N "));
     }
 }
