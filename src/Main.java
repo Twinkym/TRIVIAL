95a6a8e66f658e69942f29738310fd2a685be44f
Rosmupo <95884239+Rosmupo@users.noreply.github.com>
2022-02-16 13:42:26 +0100

    Fase 2 corregido por Alfredo

--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,31 @@ public class Main {
         List<Pregunta> preguntas = buildPreguntasList();
 
         System.out.println("las preguntas son: " + preguntas);
-        int contador = showPreguntas(preguntas);
+        int contadorPuntuacion = showPreguntasyPuntuacion(preguntas);
 
-        System.out.println(contador);
+
+        System.out.println(contadorPuntuacion);
 
     }
-
 
 
     private static List<Pregunta> buildPreguntasList() {
         List<Pregunta> preguntas = new ArrayList<>();
 
-        preguntas.add(new Pregunta("¿Java es un lenguaje tipado? T/F ", true));
-        preguntas.add(new Pregunta("¿Se pueden hacer aplicaciones móviles con Java? T/F ", true));
-        preguntas.add(new Pregunta(" ¿No se puede crear vídeo juegos con Java? T/F ", false));
-        preguntas.add(new Pregunta(" ¿Java es un lenguaje POO? T/F ", true));
-        preguntas.add(new Pregunta(" ¿El lenguaje Java fue comercializado en 1998? T/F ", false));
+        preguntas.add(new Pregunta("¿Java es un lenguaje tipado? T/F ", true, 5));
+        preguntas.add(new Pregunta("¿Se pueden hacer aplicaciones móviles con Java? T/F ", true, 5));
+        preguntas.add(new Pregunta(" ¿No se puede crear vídeo juegos con Java? T/F ", false, 5));
+        preguntas.add(new Pregunta(" ¿Java es un lenguaje POO? T/F ", true, 5));
+        preguntas.add(new Pregunta(" ¿El lenguaje Java fue comercializado en 1998? T/F ", false, 5));
 
         return preguntas;
     }
 
-    private static int showPreguntas(List<Pregunta> preguntas) {
+    private static int showPreguntasyPuntuacion(List<Pregunta> preguntas) {
 
-        int contador = 0;
+        int contadorPuntuacion = 0;
 
-        for (int i = 0; i < preguntas.size() ; i++) {
+        for (int i = 0; i < preguntas.size(); i++) {
             Pregunta currentPregunta = preguntas.get(i);
 
             System.out.println(currentPregunta.getPregunta());
@@ -47,20 +47,81 @@ public class Main {
 
             boolean respuestaUsuario = option.equalsIgnoreCase("T");
 
-            if (respuestaUsuario == currentPregunta.isRespuesta()){
-                System.out.println("respuesta correcta");
-                contador++;
+            currentPregunta.setRespuestaUsuario(respuestaUsuario);
 
+            if (respuestaUsuario == currentPregunta.isRespuestaCorrecta()) {
+                System.out.println("Felicidades! Has acertado");
             }
             else {
                 System.out.println("Nice try ;-)");
             }
 
+
+
+            if (respuestaUsuario == currentPregunta.isRespuestaCorrecta()) {
+                System.out.println("respuesta correcta");
+                contadorPuntuacion = contadorPuntuacion++;
+
+
+            } else {
+                System.out.println("Nice try ;-)");
+            }
+            System.out.println("Su puntuación es: ");
+
         }
 
-        return  contador;
+        int puntuacionTotal = 0;
+        int contadorPreguntasAcertadas = 0;
+
+        for (int i = 0; i < preguntas.size(); i++) {
+            Pregunta currentPregunta = preguntas.get(i);
+            if( currentPregunta.isRespuestaUsuario()
+                    == currentPregunta.isRespuestaCorrecta() ) {
+                System.out.println(currentPregunta.getPregunta());
+                System.out.println("has acertado: ");
+                puntuacionTotal = puntuacionTotal + currentPregunta.getPuntuacion();
+                contadorPreguntasAcertadas++;
+            }
+        }
+
+        System.out.println("has acertado: " + contadorPreguntasAcertadas );
+
+        return puntuacionTotal ;
 
     }
+
 }
-
-
+//    private static int showEstadisticas(List<Pregunta> preguntas) {
+//
+//        int acumuladorPuntuacion = 0;
+//
+//        for (int i = 0; i < preguntas.size() ; i++) {
+//            Pregunta currentPregunta = preguntas.get(i);
+//            int acumuladorPuntuacion = currentPregunta.getPuntuacion();
+//
+//
+//
+//    }
+//
+//}
+//
+///*
+//private static double getAveragePoints(List<Player> players) {
+//
+//        //return players.stream().mapToInt(PlayerClass::getNumPoints).average().getAsDouble();
+//
+//        double sumTotal = 0;
+//
+//        for (int i = 0; i < players.size(); i++) {
+//
+//            Player currentPlayer = players.get(i);
+//            double currentPoints = currentPlayer.getNumPoints();
+//            sumTotal = sumTotal + currentPoints;
+//
+// */
+//*/
+//
+// */
+//
+////currentrespuesta.setRespuestaUsuario(userytrue)
+//
--- src/Pregunta.java
+++ src/Pregunta.java
@@ -3,49 +3,60 @@ import java.util.Objects;
 public class Pregunta {
 
     private String pregunta;
-    private boolean respuesta;
+    private boolean respuestaCorrecta;
+    private boolean respuestaUsuario;
+    private int puntuacion;
 
-    public boolean isRespuesta() {
-        return respuesta;
+    public Pregunta(String pregunta, boolean respuestaCorrecta, int puntuacion) {
+        this.pregunta = pregunta;
+        this.respuestaCorrecta = respuestaCorrecta;
+        this.respuestaUsuario = false;
+        this.puntuacion = puntuacion;
+
     }
 
-    public Pregunta(String pregunta, boolean respuesta) {
+    public void setPregunta(String pregunta) {
         this.pregunta = pregunta;
-        this.respuesta = respuesta;
+    }
+
+    public void setRespuestaCorrecta(boolean respuestaCorrecta) {
+        this.respuestaCorrecta = respuestaCorrecta;
+    }
+
+    public void setRespuestaUsuario(boolean respuestaUsuario) {
+        this.respuestaUsuario = respuestaUsuario;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
     }
 
     public String getPregunta() {
         return pregunta;
     }
 
+    public boolean isRespuestaCorrecta() {
+        return respuestaCorrecta;
+    }
+
+    public boolean isRespuestaUsuario() {
+        return respuestaUsuario;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
-        Pregunta pregunta = (Pregunta) o;
-
-        return Objects.equals(this.pregunta, pregunta.pregunta);
+        Pregunta pregunta1 = (Pregunta) o;
+        return respuestaCorrecta == pregunta1.respuestaCorrecta && respuestaUsuario == pregunta1.respuestaUsuario && puntuacion == pregunta1.puntuacion && Objects.equals(pregunta, pregunta1.pregunta);
     }
 
     @Override
-    public String toString() {
-        return "Preguntas " +
-                "pregunta=' " + pregunta + '\'' + "\n"
-                ;
+    public int hashCode() {
+        return Objects.hash(pregunta, respuestaCorrecta, respuestaUsuario, puntuacion);
     }
-
-
-
-    //    @Override
-//    public int hashCode() {
-//        return pregunta != null ? pregunta.hashCode() : 0;
-//    }
-
-    //    @Override
-//    public int hashCode() {
-//        int result = pregunta != null ? pregunta.hashCode() : 0;
-//        result = 31 * result + (respuesta != null ? respuesta.hashCode() : 0);
-//        return result;
-//    }
 }
